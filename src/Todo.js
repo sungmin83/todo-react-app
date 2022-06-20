7bd57a30314962dd950203ba6b677ab35c276d08
sungmin83 <dreamfctory@naver.com>
2022-06-20 20:14:37 +0900

    Fail > app-config.js, ApiService.js

--- src/Todo.js
+++ src/Todo.js
@@ -7,36 +7,39 @@ class Todo extends React.Component {
         super(props);
         this.state = {item: props.item, readOnly: true};
         this.delete = props.delete;
+        this.update = props.update;
     }
 
     deleteEventHandler = () => {
         this.delete(this.state.item)
-    }
+    };
 
     offReadOnlyMode = () => {
         console.log("Event!", this.state.readOnly)
         this.setState({readOnly: false}, () => {
             console.log("ReadOnly? ", this.state.readOnly)
         });
-    }
+    };
 
     enterKeyEventHandler = (e) => {
         if(e.key === 'Enter'){
             this.setState({readOnly: true});
+            this.update(this.state.item);
         }
-    }
+    };
 
     editEventHandler = (e) => {
         const thisItem = this.state.item;
         thisItem.title = e.target.value;
         this.setState({item: thisItem});
-    }
+    };
 
     checkboxEventHandler = (e) => {
         const thisItem = this.state.item;
         thisItem.done = !thisItem.done;
         this.setState({item: thisItem});
-    }
+        this.update(this.state.item);
+    };
 
     render() {
         const item = this.state.item;
